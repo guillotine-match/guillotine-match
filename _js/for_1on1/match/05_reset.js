904bcf14e2cb1ecc70293d4ff87673a8295c303a
tlwns5598 <tlwns5598@gmail.com>
2021-07-12 12:56:03 +0900

    1

--- _js/for_1on1/match/05_reset.js
+++ _js/for_1on1/match/05_reset.js
@@ -2,23 +2,36 @@
 
 function resetClicked(btn_id){
 
-    //  reset_button_sound
+    //  (1) reset_button_sound
     anchor.load();
     anchor.play();
 
-    // 모든 리셋 함수
+    // (2) 승리화면 리셋
     portrait_reset();
+    
+    // (3) 오디오 중단
     audio_stop();
+    
+    // (4) 라디오 리셋
     radio_reset();
+    
+    // (5) 쉬프트박스 & 변수 리셋
     sbox_and_variable_reset();
+
+    // (6) 스크롤 리셋
     scroll_reset();
+
+    // (7) 승리 메시지 리셋
     winner_text_reset();
+
+    // (8) 오디오 출력화면 리셋
     audio_display_off();
+
+    // (9) 그라디언트 삭제
     reset_gradient();
 } 
 
 function portrait_reset(){
-    // portrait_reset
     portBox1.style.display = "block";
     portBox2.style.display = "none";
     portBox3.style.display = "none";
@@ -39,7 +52,7 @@ function sbox_and_variable_reset(){
     m1=0; m2=0; winner="an";
     matchIndex=0;
     selected_Box_num=0;
-    ShowBoxIdx();
+    boxShifter();
 }
 
 function scroll_reset(){
